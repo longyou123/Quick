5de43e1f3921f778b7c48bd4a2f6fd97c5f6dc78
longyou123 <360493323@qq.com>
2018-06-28 11:00:19 +0800

    首页

--- app/src/main/java/com/quick/quick/base/HomeActivity.java
+++ app/src/main/java/com/quick/quick/base/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.quick.quick.base;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 
@@ -17,7 +18,22 @@ public class HomeActivity  extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_home_layout);
+
+        new Thread(){
+
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(1000);
+
+                    startActivity(new Intent(HomeActivity.this,WelcomeActivity.class));
+                    finish();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }.start();
     }
 }
